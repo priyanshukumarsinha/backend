083b9224dc2549b0926b6d57ff12b898d5a1abef
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-10 16:27:36 +0530

    requestHandler defined

--- src/app.js
+++ src/app.js
@@ -5,13 +5,29 @@ import cookieParser from 'cookie-parser'
 // make an app
 const app = express()
 
+
+// SETTING UP MIDDLEWARES
 // to use a middleware or for any configuration setting, we use app.use()
 app.use((cors({// cors congig
     // we can also add some options to our cors 
     origin: process.env.CORS_ORIGIN,
     credentials : true,
-    
+
 }))) 
+
+// Now data can come from various types in backend
+// maybe from JSON, or from any FORM, etc
+// Therefore to config this :
+app.use(express.json({limit : "16kb"}))
+
+// 
+app.use(express.urlencoded({extended : true, limit : "16kb", }))
+// extended : for Nested Objects
+app.use(express.static("public"))
+
+// To keep secure Cookies and perform CRUD operation on them : 
+app.use(cookieParser())
+
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+// USING PROMISES
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+
+export {asyncHandler}
+
+// Higher Order Functions
+// const asyncHandler = (func) => {() => {}}
+// const asyncHandler = (func) => async() => {}
+
+// USING TRY CATCH
+
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success : false,
+//             message : error.message,
+//         })
+//     }
+// } 
