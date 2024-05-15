8805151ccdaaccfdd8b19740f79ae7795c0afe01
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-15 06:35:56 +0530

    Refactor user.routes.js to include new endpoints and update existing ones

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
+import { 
+            changeCurrentPassword, 
+            getCurrentUser, 
+            getUserChannelProfile, 
+            getWatchHistory, 
+            loginUser, 
+            logoutUser, 
+            refreshAccessToken, 
+            registerUser, 
+            updateAccountDetails, 
+            updateUserAvatar, 
+            updateUserCoverImage 
+} from "../controllers/user.controller.js";
+
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,10 +33,21 @@ router.route("/logout").post(verifyJWT, logoutUser)
 // http://lcalhost:3000/api/v1/users/login
 
 router.route("/refresh-token").post(refreshAccessToken)
-
+// only verified (or loggedin Users) should be able to change Password, hence use verifyJWT
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
 
-router.route('/update-account-details').post(verifyJWT, updateAccountDetails)
+// here we need to have patch, so that only some information will change 
+router.route('/update-account').patch(verifyJWT, updateAccountDetails)
+
+router.route('/update-avatar').patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route('/update-coverImage').patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile);
+
+router.route('/history').get(verifyJWT, getWatchHistory);
+
+
 export default router
 
 
