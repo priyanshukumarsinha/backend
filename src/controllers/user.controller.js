bb888fa5ed52e5e1324704a88d81434ff96f9e4e
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-17 21:42:37 +0530

    Test

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -276,6 +276,7 @@ const updateAccountDetails = asyncHandler(async(req, res) => {
         }
     ).select("-password")
 
+    
     return res.status(200)
                 .json(
                     new ApiResponse(
